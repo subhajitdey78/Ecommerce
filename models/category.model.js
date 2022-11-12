324ef50aad354520fd2c0411802281d97965f104
subhajitdey78 <107293843+subhajitdey78@users.noreply.github.com>
2022-11-12 20:21:05 +0530

    Added server.js and routes for category

--- models/category.model.js
+++ models/category.model.js
@@ -1,32 +1,29 @@
 /**
  * This file will be used to represent the category schema
  * 
- * category fields:
+ * Category fields:
  * 1. id
  * 2. name
  * 3. description
- */
+*/
 
-//Category -> categories
- module.exports = (sequelize, Sequelize) => {
+module.exports = (sequelize, Sequelize) => {
     const Category = sequelize.define("category", {
-    id : {
-        type: Sequelize.INTEGER,
-        primaryKey: true,
-        autoIncrement: true
-    },
+        id: {
+            type: Sequelize.INTEGER,
+            primaryKey: true,
+            autoIncrement: true
+        }, 
         name: {
             type: Sequelize.STRING,
             allowNull: false,
-                },
-            description: {
-                    type: Sequelize.STRING
-                }
-            }, {
-                tableName: 'categories'
-            });
-            return Category;
+        },
+        description: {
+            type: Sequelize.STRING
         }
-    
-    
-            
+    }, 
+    {
+        tableName: 'categories'
+    });
+    return Category;
+}
--- routes/category.routes.js
+++ routes/category.routes.js
@@ -1,15 +1,15 @@
 /**
- * This file will contain the routing logic for the Category controller
+ *  This file will contain the routing logic for the Category controller
  * 
- */
+*/
 
 
 const categoryController = require("../controllers/category.controller")
 
 module.exports = function(app) {
 
-    //Route for the POST request to create a category
-    app.post("/ecomm/api/v1/categories", categoryController.create);
+    // Route for the POST request to create a category
+    app.post("/ecomm/api/v1/categories",  categoryController.create);
 
     //Route for the GET request to fetch all the categories
     app.get("/ecomm/api/v1/categories", categoryController.findAll);
@@ -18,8 +18,8 @@ module.exports = function(app) {
     app.get("/ecomm/api/v1/categories/:id", categoryController.findOne);
 
     //Route for the PUT request to update a category based on id
-    app.put("/ecom/api/v1/categories/:id", categoryController.update);
+    app.put("/ecomm/api/v1/categories/:id", categoryController.update);
 
-    //Route for the DELTE request to delete a category based on the id
-    app.delete("/ecom/api/v1/categories/:id", categoryController.delete);
+    //Route for the DELETE request to delete a category based on the id
+    app.delete("/ecomm/api/v1/categories/:id",categoryController.delete);
 }
