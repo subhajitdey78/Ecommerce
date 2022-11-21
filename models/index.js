b80ed8807f411ea15c824cdd390df7ef555fc6fe
subhajitdey78 <107293843+subhajitdey78@users.noreply.github.com>
2022-11-21 10:43:18 +0530

    production value

--- models/index.js
+++ models/index.js
@@ -7,7 +7,7 @@
 * trying to import this file, just need to provide the module table
 *
 */
-const env = process.env.NODE_ENV || 'development'; //env -> Production, development
+const env = process.env.NODE_ENV || 'development'; 
 const config = require("../configs/db.config")[env];
 const Sequelize = require("sequelize");
 
